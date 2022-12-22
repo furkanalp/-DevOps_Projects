b6b5d028dca5532cbd80b22b73cbf6e37e6ee1bc
furkan <furkan.alperen08@gmail.com>
2022-12-22 13:42:51 +0100

    helm commands

---
+++ 204-Kubernetes-Microservice-Phonebook/k8s/command_for_helm.sh
@@ -0,0 +1,5 @@
+# optional
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+helm create phonebook
+rm -rf phonebook/templates/*
+helm install my-phonebook phonebook/
